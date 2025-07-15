c4574cd8eeff078ce2d0a0a59d127a2d44435e08
Aanya Mahapatra <aanya.mahapatra@gmail.com>
2025-07-15 09:17:47 -0500

    Initial Commit

---
+++ GitHubCollaboration/GitHubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationApp.swift
+//  GitHubCollaboration
+//
+//  Created by Aanya Mahapatra on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
